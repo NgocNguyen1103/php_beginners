5b1740cac19b49aa94a456150f21c17c106f8c27
NgocNguyen1103 <quangquangngoc@gmail.com>
2025-09-13 16:40:08 +0700

    Array

--- index.php
+++ index.php
@@ -6,61 +6,84 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input type="text" name="num01" placeholder="Number one"><br>
-        <select name="operator" id="operator">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select><br>
-        <input type="text" name="num02" placeholder="Number two"><br>
-        <button type="submit">Calculate</button>
-    </form>    
-
     <?php 
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
-            $operator = htmlspecialchars($_POST["operator"]);
-            $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
-            
-            //Error handler
-            $error = false;
-            if(empty($num01) || empty($num02) || empty($operator)){
-                echo "Fill in all field<br>";
-                $error = true;
-            }
-            
-            if(!is_numeric($num01) || !is_numeric($num02)){
-                echo "can only work with number <br>";
-                $error = true;
-            }
+    //Declare
+    // $fruits = array("Apple", "Banana", "Cherry");
+    // $fruits = ["Apple", "Banana", "Cherry"];
+    // echo $fruits[2];
 
-            //Calculate if no error
-            
-            if(!$error){
-                $value = 0;
-                switch($operator){
-                    case "add":
-                        $value = $num01 + $num02;
-                        break;
-                    case "subtract":
-                        $value =  $num01 - $num02;
-                        break;
-                    case "multiply":
-                        $value =  $num01 * $num02;
-                        break;
-                    case "divide":
-                        $value =  $num01 / $num02;
-                        break;
-                    default:
-                        echo "Error at something <br>";
-                        break;
-                    }
-                echo $value;
-            }
-        }
+    //add element at the end
+    // $fruits[] = "Orange";
+    // echo $fruits[3];
+
+    //Replace element
+    // echo $fruits[1];
+    // $fruits[1] = "Orange";
+    // echo $fruits[1];
+
+    //empty index
+    // unset($fruits[1]);
+
+    //remove element
+    //array_slice($fruits, 0, 1) //(array, start, No of delete)
+    
+    //print arrau
+    //print_r($fruits);
     ?>
 
+    <?php 
+    //label array
+    // $task =  [
+    //     "laundry" => "Dan", 
+    //     "wash" => "Pri", 
+    //     "trash" => "Bas", 
+    //     "Dish" => "Bel"
+    // ];
+    // // echo $task["wash"];
+    // print_r($task);
+    // echo "<br>";
+    // echo count($task);
+    // // sort($task);
+    // print_r($task);
+
+    //add element
+    // $task["nothing"] = "me";
+    // print_r($task);
+    
+    ?>
+
+    <?php 
+    
+    $fruits = ["Apple", "Banana", "Cherry"];
+    $test = ["Mango", " Strawberry"];
+    //merger array
+    // array_splice($fruits, 2, 0, $test); //(arr, start, No of delete, new data)
+    // print_r($fruits)
+    
+    ?>
+
+
+    <?php
+    //multi-dimention array
+
+
+    $fruits = [
+        array("Apple", "Strawberry"),
+        "Banana",
+        'Cherry',
+    ];
+
+    // echo $fruits[0][1]; //Strawberry
+    // echo $fruits[1]; //Banana
+
+
+    $food = [
+        "fruits" => ["apple", "banana"],
+        "meat" => ["chicken", "fish"],
+        "vegetable" => ["cucumber", "carrot"],
+    ];
+    echo $food["vegetable"][0];
+    ?>
+    
 </body>
 </html>
